cb023e1cefac7f828016fa8a8be603a2920670a5
Diego Amicabile <diego.amicabile@gmail.com>
2023-01-25 14:23:30 +0100

    video merger draft

---
+++ main.py
@@ -0,0 +1,6 @@
+
+# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+import ffmpy
+INPUT_FILE='/media/diego/One Touch/videos/PLNP_nRm4k4jfAAOVb1-W3fTcsE1A71g9n/1_rXKy8CCT1Ao.mp4'
+OUTPUT_FILE='out.mp4'
+
