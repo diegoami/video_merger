7a3358aee948baf99305ab48e8ca0f708dea08d9
Diego Amicabile <diego.amicabile@gmail.com>
2023-02-02 23:27:11 +0100

    Two mergers

--- main.py
+++ main.py
@@ -1,6 +1,15 @@
 
 # See PyCharm help at https://www.jetbrains.com/help/pycharm/
 import ffmpy
-INPUT_FILE='/media/diego/One Touch/videos/PLNP_nRm4k4jfAAOVb1-W3fTcsE1A71g9n/1_rXKy8CCT1Ao.mp4'
+INPUT_FILE='/media/diego/one_touch/videos/PLNP_nRm4k4jc1yxjDO_pR4-v5_2pVRZwT/1_mlIZyNklCpY'
 OUTPUT_FILE='out.mp4'
 
+# ffmpeg -i input.mkv -filter:v "setpts=PTS/60" output.mkv
+
+
+import ffmpy
+ff = ffmpy.FFmpeg(
+     inputs={INPUT_FILE: None},
+     outputs={OUTPUT_FILE: '-filter:v "setpts=PTS/60"'}
+)
+ff.run()
